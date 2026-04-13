18734bf8dc19c14d7c72c050c02ff988c90ee671
jmartinifp <153239640+jmartinifp@users.noreply.github.com>
2026-04-13 20:40:40 +0200

    Update Config.js

--- assets/config/Config.js
+++ assets/config/Config.js
@@ -3,7 +3,10 @@ export class Config
     constructor()
     {
          this.urlPastillas="http://localhost:9876/pastillas/1";
-         this.urlUsuario="http://localhost:9876/usuario/1";
+        this.urlPastillas="https://raw.githubusercontent.com/jmartinifp/epaswebser/refs/heads/main/api_estatica/pastillas.json";
+        
+        this.urlUsuario="http://localhost:9876/usuario/1";
+        
 
     }
     getUrlPastillas()
